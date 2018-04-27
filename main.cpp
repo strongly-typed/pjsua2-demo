b46e181bb12f14744a480098b8a81957bbca7497
Sascha Schade (strongly-typed) <stronglytyp3d@gmail.com>
2018-04-27 14:48:08 +0200

    Set Null dev for sound and increase logging.

--- main.cpp
+++ main.cpp
@@ -23,8 +23,19 @@ public:
         std::cout << "*** onIncomingCall callId=" << iprm.callId << std::endl;
         Call *call = new MyCall(*this, iprm.callId);
 
+        CallInfo callInfo = call->getInfo();
+        std::cout << "    accId = " << callInfo.accId << std::endl;
+        std::cout << "    remAudioCount = " << callInfo.remAudioCount << std::endl;
+        std::cout << "    remVideoCount = " << callInfo.remVideoCount << std::endl;
+
         CallOpParam op;
-        op.statusCode = PJSIP_SC_OK;
+        op.opt.audioCount = 1;
+        op.opt.videoCount = 0;
+        op.statusCode = PJSIP_SC_RINGING;
+
+        std::cout << "Sleep ... " << std::endl;
+        pj_thread_sleep(1000);
+        std::cout << "Answer ... " << std::endl;
         call->answer(op);
 
         delete call;
@@ -43,8 +54,8 @@ int main() {
     // Init endpoint
     EpConfig ep_cfg;
     ep_cfg.uaConfig.stunServer.push_back(sip_stun_uri);
-    ep_cfg.logConfig.level = 5;
-    ep_cfg.logConfig.consoleLevel = 5;
+    ep_cfg.logConfig.level = 6;
+    ep_cfg.logConfig.consoleLevel = 6;
     ep.libInit(ep_cfg);
 
     TransportConfig tcfg;
@@ -57,9 +68,11 @@ int main() {
         return 1;
     }
 
-    // Start the library
-    ep.libStart();
-    std::cout << "*** PJSUA2 STARTED ***" << std::endl;
+    /* Use Null Audio Device as main media clock. This is useful for improving
+     * media clock (see also https://trac.pjsip.org/repos/wiki/FAQ#tx-timing)
+     * especially when sound device clock is jittery.
+     */
+    ep.audDevManager().setNullDev();
 
     // Get credentials from environment
     const char* sip_id_uri = std::getenv("SIP_ID_URI");
@@ -76,15 +89,24 @@ int main() {
     AccountConfig acfg;
     acfg.idUri = sip_id_uri;
     acfg.regConfig.registrarUri = sip_registrar_uri;
+    acfg.regConfig.retryIntervalSec = 300;
+    acfg.regConfig.firstRetryIntervalSec = 60;
     AuthCredInfo cred("digest", "*", sip_username, 0, sip_password);
     acfg.sipConfig.authCreds.push_back(cred);
+    acfg.natConfig.mediaStunUse = PJSUA_STUN_USE_DEFAULT;
 
     MyAccount *acc = new MyAccount;
     acc->create(acfg);
 
+    // Start the library
+    ep.libStart();
+    std::cout << "*** PJSUA2 STARTED ***" << std::endl;
+
     pj_thread_sleep(100000);
 
     delete acc;
+
+    ep.libDestroy();
 
     return 0;
 }
